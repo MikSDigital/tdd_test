119adf5c732046c1a624d1e5086abf79e585509e
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2017-03-25 18:19:38 +0200

    Two basic tests added

--- tests/unit/SampleTest.php
+++ tests/unit/SampleTest.php
@@ -4,5 +4,9 @@ use PHPUnit\Framework\TestCase;
 
 class SampleTest extends TestCase
 {
-
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
 }

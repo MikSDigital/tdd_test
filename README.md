e5acd0c6a879f1742032c71eb362aa92f94c114f
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2017-03-26 17:03:42 +0300

    Typo fixed in UserTest class

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Basic unit testing with PHPUnit
 Source: https://www.codecourse.com/lessons/unit-testing-with-php-unit/1302
-  To run it: vagrant@homestead:~/Code/tdd$ ./vendor/bin/phpunit
+<p>To run it: vagrant@homestead:~/Code/tdd$ ./vendor/bin/phpunit</p>
  
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -3,7 +3,18 @@
 
 class UserTest extends \PHPUnit\Framework\TestCase
 {
-    public function tetsThatWeCanGetTheFirstName() {
+    public function testThatWeCanGetTheFirstName() {
+
+
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+
+        $user = new \App\Models\User;
+
+        $user->setFirstName('Billy');
+
+        $this->assertEquals($user->getFirstName(), 'Billy');
+
 
     }
 

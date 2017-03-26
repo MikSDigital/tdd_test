01ee59a6f2d0824a2bff03a9b50173cf9bda1902
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2017-03-26 17:39:39 +0300

    testFirstAndLastNameAreTrimmed added

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,14 +5,41 @@ namespace App\Models;
 class User
 {
     public $first_name;
+    public $last_name;
 
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * @param mixed $last_name
+     */
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+    public function getFirstName() {
+
+//        return 'Billy';
+        return $this->first_name;
+    }
     public function setFirstName($firstname) {
 
         $this->first_name = $firstname;
 
     }
 
-    public function getFirstName() {
-        return 'Billy';
+    public function getFullName() {
+//        return 'Billy Clinton';
+
+//        return $this->getFirstName() . ' ' . $this->getLastName();
+
+        return $this->first_name . ' ' . $this->last_name;
+//        return "$this->first_name $this->last_name";
     }
+
 }
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -5,17 +5,39 @@ class UserTest extends \PHPUnit\Framework\TestCase
 {
     public function testThatWeCanGetTheFirstName() {
 
-
-        $this->assertTrue(true);
-        $this->assertFalse(false);
-
         $user = new \App\Models\User;
 
         $user->setFirstName('Billy');
 
         $this->assertEquals($user->getFirstName(), 'Billy');
-
-
     }
 
+    public function testThatWeCanGetTheLastName() {
+
+        $user = new \App\Models\User;
+
+        $user->setLastName('Clinton');
+
+        $this->assertEquals($user->getLastName(), 'Clinton');
+    }
+
+    public function testFullNameIsReturned() {
+
+        $user = new \App\Models\User;
+        $user->setFirstName('Billy');
+        $user->setLastName('Clinton');
+
+        $this->assertEquals($user->getFullName(), 'Billy Clinton');
+    }
+
+    public function testFirstAndLastNameAreTrimmed()
+    {
+        $user = new \App\Models\User;
+        $user->setFirstName('    Billy ');
+        $user->setLastName('                   Clinton   ');
+
+        $this->assertEquals($user->getFirstName(), 'Billy');
+        $this->assertEquals($user->getLastName(), 'Clinton');
+
+    }
 }

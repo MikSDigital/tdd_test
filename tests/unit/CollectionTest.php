e862cde2c497bbbb53184aecea4b80625e9fced0
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2017-03-26 19:27:58 +0300

    Method get() added to Collection

--- tests/unit/CollectionTest.php
+++ tests/unit/CollectionTest.php
@@ -2,5 +2,11 @@
 
 class CollectionTest extends PHPUnit\Framework\TestCase
 {
+    /** @test */
+    public function empty_instantiated_collection_returns_no_items()
+    {
+        $collection = new App\Support\Collection;
 
+        $this->assertEmpty($collection->get());
+    }
 }
